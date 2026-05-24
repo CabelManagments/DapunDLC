467425b2be460c5f2efca0158ac3d13632ff5d6f
CabelManagments <superswaggyman55@gmail.com>
2026-05-24 17:40:22 +0300

    возможно новый коммит

--- src/main/java/com/dapun/dlc/events/EventHandler.java
+++ src/main/java/com/dapun/dlc/events/EventHandler.java
@@ -2,7 +2,10 @@ package com.dapun.dlc.events;
 
 import com.dapun.dlc.DapunDLC;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.util.ActionResult;
 
 public class EventHandler {
@@ -16,28 +19,28 @@ public class EventHandler {
                     && !player.isOnGround()
                     && !player.isClimbing()
                     && !player.isTouchingWater()
-                    && !player.hasStatusEffect(net.minecraft.entity.effect.StatusEffects.BLINDNESS)
+                    && !player.hasStatusEffect(StatusEffects.BLINDNESS)
                     && player.getAttacker() == null
                     && player.getVelocity().y < 0;
 
-            float damage = (float) player.getAttributeValue(
-                    net.minecraft.entity.attribute.EntityAttributes.ATTACK_DAMAGE);
+            float dmg = (float) player.getAttributeValue(EntityAttributes.ATTACK_DAMAGE);
+            if (isCrit) dmg *= 1.5f;
 
-            if (isCrit) damage *= 1.5f;
+            float tx = (float) target.getX();
+            float ty = (float)(target.getY() + target.getHeight());
+            float tz = (float) target.getZ();
 
-            float targetX = (float) target.getX();
-            float targetY = (float) (target.getY() + target.getHeight());
-            float targetZ = (float) target.getZ();
+            MinecraftClient client = MinecraftClient.getInstance();
 
-            if (DapunDLC.config.hitParticlesEnabled) {
-                DapunDLC.hitParticles.spawnParticles(targetX, targetY, targetZ, isCrit);
-            }
+            if (DapunDLC.config.hitParticlesEnabled)
+                DapunDLC.hitParticles.spawnParticles(tx, ty, tz, isCrit);
 
-            if (DapunDLC.config.damageNumbersEnabled) {
-                DapunDLC.damageNumbers.addNumber(damage, targetX, targetY, targetZ, isCrit, false);
-            }
+            if (DapunDLC.config.damageNumbersEnabled)
+                DapunDLC.damageNumbers.addNumber(dmg, tx, ty, tz, isCrit, false);
 
             DapunDLC.targetHUD.setTarget(target);
+            DapunDLC.targetESP.setTarget(target);
+            DapunDLC.hitSound.play(client, isCrit);
 
             return ActionResult.PASS;
         });
